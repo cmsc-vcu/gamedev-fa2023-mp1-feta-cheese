3010899d91408774dcde97cf5272beadb3a6157a
hunterpanda <matthewzxu@gmail.com>
2023-10-09 02:59:10 -0400

    moving faucet location

--- Jerry's Cheese Chase/Assets/Scripts/PlayerMovement.cs
+++ Jerry's Cheese Chase/Assets/Scripts/PlayerMovement.cs
@@ -113,6 +113,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
+        Debug.Log(Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer));
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
